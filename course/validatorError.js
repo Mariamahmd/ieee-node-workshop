5f4c2415feb53d2ffdf5e9bee5459b85c13238da
Mariam Ahmed <141421025+Mariamahmd@users.noreply.github.com>
2024-03-22 14:45:55 +0200

    Delete course directory

--- course/validatorError.js
+++
@@ -1,9 +0,0 @@
-const   {validationResult}= require("express-validator");
-const validatorError = (req, res, next) => {
-  const errors =  validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({
-      errors: errors.array(),
-    })};next();
-};
-module.exports = validatorError;
